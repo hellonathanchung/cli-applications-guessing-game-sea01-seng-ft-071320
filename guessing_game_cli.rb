0e694e967807c514f3adbbb91c47509e7419283c
Nathan Chung <chung.nathanh@gmail.com>
2020-07-08 18:33:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6!"
   input = gets.chomp 
   
-    if input == random
+    if input === random
       puts "You guessed the correct number!"
     elsif input.downcase == "exit!"
       puts "Goodbye!" 
